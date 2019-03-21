ff4bc3362f312fe50169c89ae4c217b83b8ce03f
michon jordane <jordane.michon@gmail.com>
2019-03-21 15:00:34 +0100

    update Product model

--- src/product/product.model.js
+++ src/product/product.model.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import mongoose from 'mongoose';
 
 /**
@@ -17,12 +18,35 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    dominantColor: {
+      type: String,
+    },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
+
+/**
+ * Product Model
+ */
 
 const ProductModel = mongoose.model('Product', ProductSchema);
 
 ProductModel.add = productToAdd => productToAdd.save();
+
+ProductModel.getAll = () => ProductModel.find({});
+
+ProductModel.findWithoutDominantColor = () =>
+  ProductModel.find({ dominantColor: { $exists: false } });
+
+ProductModel.setDominantColor = (code, color) =>
+  ProductModel.findOneAndUpdate(
+    { productCode: code },
+    { dominantColor: color },
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 
 export default ProductModel;
